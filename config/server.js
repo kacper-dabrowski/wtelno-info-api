03708ee5458e2697432fdb4abf7897994d75d827
Kacper <kdtw301@gmail.com>
2020-09-13 20:42:28 +0200

    Admin panel - Path change

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   admin: {
+    url: "/dungeon",
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
     },
